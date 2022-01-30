e3b56a4b9a0c5efc487f84a6aba0d489cee666dc
Artem Kopytok <13ar.bass@gmail.com>
2022-01-30 13:36:07 +0300

    updated

--- cta-button.js
+++ cta-button.js
@@ -1,1 +1,11 @@
-console.log('Hi hi !');
+let triggerSection = document.querySelector('#second-section');
+let menuCtaButton = document.querySelector('#menu-cta-button');
+
+window.addEventListener('scroll', function () {
+    let distanceData = Math.round(triggerSection.getBoundingClientRect().y);
+    if (distanceData < 0) {
+        menuCtaButton.classList.remove('main-page-specific-button');
+    } else {
+        menuCtaButton.classList.add('main-page-specific-button');
+    }
+})
